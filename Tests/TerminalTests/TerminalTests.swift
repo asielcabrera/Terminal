39f7b6f9a9344632eb8a1a4ef357ef0fb5542e44
Asiel Cabrera <cabrerasiel@gmail.com>
2023-05-18 19:53:27 -0400

    Initial Commit

---
+++ Tests/TerminalTests/TerminalTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Terminal
+
+final class TerminalTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Terminal().text, "Hello, World!")
+    }
+}
